d89ccdd569c0c98a4cfc53f8abb8beaf8d2b7649
Estudiante <Estudiante@10.203.1.205>
2018-02-27 08:01:03 -0500

    Version 1.0 Hola_mundo

--- src/edu/unal/poo/conjuntoLibros/modelo/ConjuntoLibro.java
+++ src/edu/unal/poo/conjuntoLibros/modelo/ConjuntoLibro.java
@@ -78,5 +78,29 @@ public class ConjuntoLibro {
       return mayorCalificacion;
     }
     
+    public Libro buscarMenorCalificado(){
+    Libro menorCalificacion = null;
+    for(int i=0;i<20;i++){
+        if(this.libros[i]!=null && menorCalificacion == null){
+            menorCalificacion = this.libros[i];
+            }else{
+            if(this.libros[i]!= null && this.libros[i].getCalificacion() < menorCalificacion.getCalificacion()){
+                menorCalificacion=this.libros[i];                
+                }        
+            }       
+        } 
+        return menorCalificacion;
+    }
     
+    
+    public boolean calificarLibro(String titulo, int valor){
+    boolean temp = false;
+        for (int i = 0; i < 20; i++) {
+            if(this.libros[i]!= null && this.libros[i].getTitulo().equals(titulo)){
+            this.libros[i].setCalificacion(valor);
+            temp = true;
+            }          
+        }  
+    return temp;    
+    }   
 }
